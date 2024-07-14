744e312376f1def43d01d2fde3189bb513826597
JohnAle22 <jaguerrac@uce.edu.ec>
2024-07-14 00:32:54 -0500

    Se agregan excepciones a los formularios de registro y login

--- src/app.py
+++ src/app.py
@@ -9,9 +9,11 @@ app.secret_key = 'your_secret_key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '1234'
 app.config['MYSQL_DB'] = 'prueba1' #Base de datos
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#Inicializar la extension de MySQL
 mysql = MySQL(app)
 
 client = MongoClient("mongodb://localhost:27017/")
@@ -31,18 +33,21 @@ def acceso_login():
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
         _correo = request.form['email']
         _password = request.form['password']
-                
-        cur=mysql.connection.cursor()
-        cur.execute('SELECT * FROM usuarios WHERE email = %s AND password = %s', (_correo, _password,))
-        account = cur.fetchone()
-        
-        if account:
-            session['logueado'] = True
-            session['id'] = account['id']
-            return render_template("ingresar.html")
-        else:
-            return render_template('login.html')
-
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * FROM usuarios WHERE email = %s AND password = %s", (_correo, _password))
+            account = cur.fetchone()
+            cur.close()
+            
+            if account:
+                session['logueado'] = True
+                session['id'] = account['id']
+                return render_template("ingresar.html")
+            else:
+                return render_template('login.html', error='Credenciales inválidas. Por favor, intenta nuevamente.')
+        except Exception as e:
+            return str(e)
+    return render_template('login.html')
 
 @app.route('/ingresar', methods=['GET'])
 def ingresar():
@@ -52,19 +57,28 @@ def ingresar():
 def registro():
         return render_template('registro.html')
     
-@app.route('/crear-registro', methods = ['GET', 'POST'])
+@app.route('/crear_registro', methods = ['GET', 'POST'])
 def crear_registro():
-    firstname = request.form['firstname']
-    lastname = request.form['lastname']
-    phone = request.form['phone']
-    country = request.form['country']
-    email = request.form['email']
-    password = request.form['password']
-    
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO usuarios (firstname, lastname, phone, country, email, password) VALES (%s, %s, %s, %s, %s, %s)", (firstname, lastname, phone, country, email, password))
-    mysql.connection.commit()   
-    return render_template('intereses.html')
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        phone = request.form['phone']
+        country = request.form['country']
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute(
+                "INSERT INTO usuarios (firstname, lastname, phone, country, email, password) VALUES (%s, %s, %s, %s, %s, %s)",
+                (firstname, lastname, phone, country, email, password)
+            )
+            mysql.connection.commit()
+            cur.close()
+            return render_template('intereses.html')
+        except Exception as e:
+            return str(e)
+
+    return render_template('crear_registro.html')
 
 @app.route('/intereses', methods=['GET', 'POST'])
 def intereses():
